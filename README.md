39799a613e586d263f4f9881508b45eb664f68cc
vaguettibfd <vaguetti.bfd@gmail.com>
2026-05-27 21:01:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# aula27052026
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/vaguettibfd/aula27052026)
---
+++ tipovar.js
@@ -0,0 +1,13 @@
+var numero; 
+console.log(typeof(numero));
+numero = 5.2;
+console.log(typeof(numero));
+
+var palavra = "JavaScript";
+console.log(typeof(palavra));
+
+var logica = false;
+console.log(typeof(logica));
+
+var qualquer;
+console.log(typeof(qualquer));
